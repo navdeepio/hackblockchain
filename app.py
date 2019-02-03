efde2fd3e8eec039272db82eaedd4bbecd8c0412
Nav Saini <navdeep@mailbox.org>
2019-02-03 12:27:50 +0530

    index page working

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 #! /bin/env python
 from flask import Flask, render_template, request
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
+
+
+# homepage
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
 # login user
